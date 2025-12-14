b75754436baf414e612d78f83ae824fb0458f4cd
thissidemayur <thissidemayur@gmail.com>
2025-12-14 09:26:40 +0530

    initalize simple node project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return(
+    <>
+    <h1>Github Actions</h1>
+    <h2>Jai ho</h2>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js (or vite.config.ts)
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.js"],
+  },
+});
